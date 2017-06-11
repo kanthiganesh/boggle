be7f7736b3e62c494853c9ae6ec95d1a8b51a747
Ganesh <ganesh.kanthiwar@gmail.com>
2017-06-11 22:28:06 +0530

    privacypolicy with content

---
+++ site.php
@@ -0,0 +1,6 @@
+<div class="copyright">
+<div style="float: left;margin-top: 5px;margin-left: 20px;"><a href="siteterms.php"><span style="color:white;">Site Terms </span></a>|<a href="privacy.php"><span style="color:white;"> Privacy</span></a></div>
+    <div class="container">
+       <p>All Rights Reserved. Boggle Pvt Ltd@2017</p>
+   </div>
+</div>
